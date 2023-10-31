967ad4edef2f70cb1c5b6681ac4e1b7c6d382caf
Nikita Teremovskiy <ni-2@mail.ru>
2023-10-31 13:06:08 +0300

    feat: add check for x.com

--- oracle/verify-github.js
+++ oracle/verify-github.js
@@ -1,34 +1,34 @@
-import * as puppeteer from 'puppeteer'
+import * as puppeteer from "puppeteer";
 
 export const verifyGitHub = async (verificationPackage) => {
-  const [gitHubAccount, proofUrl, anotherAccount] = verificationPackage
-  const [username] = gitHubAccount.split('/')
-  const [anotherUsername] = anotherAccount.split('/')
+    const [gitHubAccount, proofUrl, anotherAccount] = verificationPackage;
+    const [username] = gitHubAccount.split("/");
+    const [anotherUsername] = anotherAccount.split("/");
 
-  if (proofUrl.indexOf('https://github.com') !== 0) {
-      console.log(`Invalid proof URL for Twitter: "${proofUrl}".`)
-      return false
-  }
-  console.log(
-      `Processing connection "${anotherAccount}" <=> "${gitHubAccount}" with proof: ${proofUrl}`
-  )
-  console.log(`Browser launching...`)
+    if (proofUrl.indexOf("https://github.com") !== 0) {
+        console.log(`Invalid proof URL for GitHub: "${proofUrl}".`);
+        return false;
+    }
+    console.log(
+        `Processing connection "${anotherAccount}" <=> "${gitHubAccount}" with proof: ${proofUrl}`
+    );
+    console.log(`Browser launching...`);
 
-  const browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  })
-  const page = await browser.newPage()
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
 
-  console.log(`Browser launched.`)
+    console.log(`Browser launched.`);
 
-  await page.goto(proofUrl, { waitUntil: 'networkidle2', timeout: 60000 })
-  let title = await page.title()
-  title = title.toLowerCase()
-  console.log(`Downloaded page "${title}".`)
+    await page.goto(proofUrl, { waitUntil: "networkidle2", timeout: 60000 });
+    let title = await page.title();
+    title = title.toLowerCase();
+    console.log(`Downloaded page "${title}".`);
 
-  return (
-      title.indexOf(username.toLowerCase()) !== -1 &&
-      title.indexOf(anotherUsername.toLowerCase()) !== -1
-  )
-}
+    return (
+        title.indexOf(username.toLowerCase()) !== -1 &&
+        title.indexOf(anotherUsername.toLowerCase()) !== -1
+    );
+};
--- oracle/verify-twitter.js
+++ oracle/verify-twitter.js
@@ -5,10 +5,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const xpaths = {
-  modal_header: '//*[@id="modal-header"]/span/span',
-  modal_input: '//*[@id="layers"]//input',
-  modal_next_button: '//*[@id="layers"]//span[text()="Next"]',
-  modal_login_button: '//*[@id="layers"]//span[text()="Log in"]',
+    modal_header: '//*[@id="modal-header"]/span/span',
+    modal_input: '//*[@id="layers"]//input',
+    modal_next_button: '//*[@id="layers"]//span[text()="Next"]',
+    modal_login_button: '//*[@id="layers"]//span[text()="Log in"]',
 };
 const defaultTypeOptions = { delay: 30 };
 const defaultWaitForSelectorOptions = { visible: true, timeout: 30000 };
@@ -16,99 +16,102 @@ const defaultWaitForSelectorOptions = { visible: true, timeout: 30000 };
 const sleep = (waitTimeInMs) => new Promise((resolve) => setTimeout(resolve, waitTimeInMs));
 
 export const verifyTwitter = async (verificationPackage) => {
-  const [twitterAccount, proofUrl, anotherAccount] = verificationPackage;
-  const [username] = twitterAccount.split("/");
-  const [anotherUsername] = anotherAccount.split("/");
+    const [twitterAccount, proofUrl, anotherAccount] = verificationPackage;
+    const [username] = twitterAccount.split("/");
+    const [anotherUsername] = anotherAccount.split("/");
 
-  if (proofUrl.indexOf("https://twitter.com") !== 0) {
-    console.log(`Invalid proof URL for Twitter: "${proofUrl}".`);
-    return false;
-  }
-  console.log(
-    `Processing connection "${anotherAccount}" <=> "${twitterAccount}" with proof: ${proofUrl}`
-  );
-  console.log(`Browser launching...`);
-
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-  const page = await browser.newPage();
-
-  if (fs.existsSync("cookies.json")) {
-    const cookies = fs.readFileSync("cookies.json", "utf8");
-    const deserializedCookies = JSON.parse(cookies);
-    await page.setCookie(...deserializedCookies);
-  }
-
-  console.log(`Browser launched.`);
-
-  await page.goto(proofUrl, { waitUntil: "networkidle2", timeout: 120000 });
-
-  const getTitle = async () => {
-    let title = await page.title();
-    title = title.toLowerCase();
-    console.log(`Downloaded page "${title}".`);
-    return title;
-  };
-
-  const title = await getTitle();
-
-  const finish = async (message) => {
-    await browser.close();
-    return (
-      message.indexOf("@" + username.toLowerCase()) !== -1 &&
-      message.indexOf(anotherUsername.toLowerCase()) !== -1
+    if (proofUrl.indexOf("https://twitter.com") !== 0 && proofUrl.indexOf("https://x.com") !== 0) {
+        console.log(`Invalid proof URL for X: "${proofUrl}".`);
+        return false;
+    }
+    console.log(
+        `Processing connection "${anotherAccount}" <=> "${twitterAccount}" with proof: ${proofUrl}`
     );
-  };
+    console.log(`Browser launching...`);
 
-  if (title.indexOf("log in") === -1) {
-    return finish(title);
-  }
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
 
-  // If the authorisation page was opened we should authorise the oracle user
+    if (fs.existsSync("cookies.json")) {
+        const cookies = fs.readFileSync("cookies.json", "utf8");
+        const deserializedCookies = JSON.parse(cookies);
+        await page.setCookie(...deserializedCookies);
+    }
 
-  const emailInput = await page.waitForXPath(xpaths.modal_input, defaultWaitForSelectorOptions);
-  await emailInput.type(process.env.TWITTER_EMAIL, defaultTypeOptions);
+    console.log(`Browser launched.`);
 
-  const nextButton = await page.waitForXPath(xpaths.modal_next_button, { visible: true });
-  await nextButton.click();
+    await page.goto(proofUrl, { waitUntil: "networkidle2", timeout: 120000 });
 
-  const modal = await page.waitForXPath(xpaths.modal_header, defaultWaitForSelectorOptions);
-  const usernameText = await page.evaluate((el) => el.textContent, modal);
+    const getTitle = async () => {
+        let title = await page.title();
+        title = title.toLowerCase();
+        console.log(`Downloaded page "${title}".`);
+        return title;
+    };
 
-  if (usernameText.includes("Enter your phone number or username")) {
-    const usernameInput = await page.waitForXPath(
-      xpaths.modal_input,
-      defaultWaitForSelectorOptions
+    const title = await getTitle();
+
+    const finish = async (message) => {
+        await browser.close();
+        return (
+            message.indexOf("@" + username.toLowerCase()) !== -1 &&
+            message.indexOf(anotherUsername.toLowerCase()) !== -1
+        );
+    };
+
+    if (title.indexOf("log in") === -1) {
+        return finish(title);
+    }
+
+    // If the authorisation page was opened we should authorise the oracle user
+
+    const emailInput = await page.waitForXPath(xpaths.modal_input, defaultWaitForSelectorOptions);
+    await emailInput.type(process.env.TWITTER_EMAIL, defaultTypeOptions);
+
+    const nextButton = await page.waitForXPath(xpaths.modal_next_button, { visible: true });
+    await nextButton.click();
+
+    const modal = await page.waitForXPath(xpaths.modal_header, defaultWaitForSelectorOptions);
+    const usernameText = await page.evaluate((el) => el.textContent, modal);
+
+    if (usernameText.includes("Enter your phone number or username")) {
+        const usernameInput = await page.waitForXPath(
+            xpaths.modal_input,
+            defaultWaitForSelectorOptions
+        );
+        await usernameInput.type(process.env.TWITTER_USERNAME, defaultTypeOptions);
+
+        const nextButtonVerify = await page.waitForXPath(
+            xpaths.modal_next_button,
+            defaultWaitForSelectorOptions
+        );
+        await nextButtonVerify.click();
+    }
+
+    const passwordInput = await page.waitForXPath(
+        xpaths.modal_input,
+        defaultWaitForSelectorOptions
     );
-    await usernameInput.type(process.env.TWITTER_USERNAME, defaultTypeOptions);
+    await passwordInput.type(process.env.TWITTER_PASSWORD, defaultTypeOptions);
 
-    const nextButtonVerify = await page.waitForXPath(
-      xpaths.modal_next_button,
-      defaultWaitForSelectorOptions
+    const nextButtonLogin = await page.waitForXPath(
+        xpaths.modal_login_button,
+        defaultWaitForSelectorOptions
     );
-    await nextButtonVerify.click();
-  }
+    await nextButtonLogin.click();
 
-  const passwordInput = await page.waitForXPath(xpaths.modal_input, defaultWaitForSelectorOptions);
-  await passwordInput.type(process.env.TWITTER_PASSWORD, defaultTypeOptions);
+    await sleep(3000);
 
-  const nextButtonLogin = await page.waitForXPath(
-    xpaths.modal_login_button,
-    defaultWaitForSelectorOptions
-  );
-  await nextButtonLogin.click();
+    // Go back to the checking page, save cookies and get title
 
-  await sleep(3000);
+    await page.goto(proofUrl, { waitUntil: "networkidle2", timeout: 120000 });
 
-  // Go back to the checking page, save cookies and get title
+    const cookies = await page.cookies();
+    fs.writeFileSync("cookies.json", JSON.stringify(cookies));
 
-  await page.goto(proofUrl, { waitUntil: "networkidle2", timeout: 120000 });
-
-  const cookies = await page.cookies();
-  fs.writeFileSync("cookies.json", JSON.stringify(cookies));
-
-  const title2 = await getTitle();
-  return finish(title2);
+    const title2 = await getTitle();
+    return finish(title2);
 };
